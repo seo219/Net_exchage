809b05e91018b8882bf01b94f3f56ecdde26a977
seo219 <seo920199@gmail.com>
2025-11-26 14:12:59 +0900

    first commit

---
+++ netlify/functions/exchange.js
@@ -0,0 +1,22 @@
+exports.handler = async (event, context) => {
+    const apiKey = ProcessingInstruction.env.EXCHANGE_API_KEY;
+    const {from} = event.queryStringParameters;
+
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${from}`;
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error: error.message}),
+        };
+    }
+}
